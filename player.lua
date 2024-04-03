60635318193a3ad8b1ae4abe13260fb0c8bf6582
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-04-04 01:10:48 +0200

    Add check for boundaries and load default Player values

--- player.lua
+++ player.lua
@@ -6,15 +6,8 @@ Player = {
    speed
 }
 
-function Player:load()
-   self.x = 50
-   self.y = love.graphics.getHeight() / 2
-   self.width = 20
-   self.height = 100
-   self.speed = 500
-end
-
-function Player:update(dt)
+-- Helper functions
+function Player:move(dt)
    if love.keyboard.isDown("w") or love.keyboard.isDown("z") then
       self.y = self.y - self.speed * dt
    elseif love.keyboard.isDown("s") then
@@ -22,7 +15,29 @@ function Player:update(dt)
    end
 end
 
+function Player:checkBoundaries()
+   if self.y < 0 then
+      self.y = 0
+   elseif self.y + self.height > WINDOW_HEIGHT then
+      self.y = WINDOW_HEIGHT - self.height
+   end
+end
+
+-- Core functions
+function Player:load()
+   self.width = 20
+   self.height = 100
+   self.x = 50
+   self.y = (WINDOW_HEIGHT) / 2 - (self.height / 2)
+   self.speed = 500
+end
+
+function Player:update(dt)
+   Player:move(dt)
+   Player:checkBoundaries(dt)
+end
+
 function Player:draw()
    love.graphics.setColor(1, 1, 1)
-   love.graphics.rectangle("fill", self.x, self.y - (self.height / 2), self.width, self.height)
+   love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 end
