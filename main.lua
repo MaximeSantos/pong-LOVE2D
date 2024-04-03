db03a9a61fa7080f717d7e104e0e0e58465ad011
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-04-03 18:17:30 +0200

    Add player movement

--- main.lua
+++ main.lua
@@ -8,13 +8,22 @@ function love.load()
    love.window.setTitle("Pong LÖVE2D")
    love.graphics.setDefaultFilter("nearest", "nearest")
    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {fullscreen = false, resizable = false, vsync = true})
+
+   -- Load the player
+   Player:load()
 end
 
 function love.update(dt)
+   -- Update the player
+   Player:update(dt)
 end
 
 function love.draw()
+   -- Draw some text in the center of the screen
    love.graphics.setColor(1, 1, 1)
    love.graphics.printf("Hello World !", 0, WINDOW_HEIGHT / 2 - 16, WINDOW_WIDTH, "center")
    love.graphics.printf("Welcome to Pong in lua with LÖVE2D", 0, WINDOW_HEIGHT / 2, WINDOW_WIDTH, "center")
+
+   -- Draw the player
+   Player:draw()
 end
--- player.lua
+++ player.lua
@@ -1,16 +1,28 @@
 Player = {
-   x = 100,
-   y = 100,
-   velocity = 0
+   x,
+   y,
+   width,
+   height,
+   speed
 }
 
-function Player.load()
+function Player:load()
+   self.x = 50
+   self.y = love.graphics.getHeight() / 2
+   self.width = 20
+   self.height = 100
+   self.speed = 500
 end
 
-function Player.update(dt)
+function Player:update(dt)
+   if love.keyboard.isDown("w") or love.keyboard.isDown("z") then
+      self.y = self.y - self.speed * dt
+   elseif love.keyboard.isDown("s") then
+      self.y = self.y + self.speed * dt
+   end
 end
 
-function Player.draw()
-   love.graphics.setColor(1, 0, 0)
-   love.graphics.rectangle("fill", self.x, self.y, 10, 100)
+function Player:draw()
+   love.graphics.setColor(1, 1, 1)
+   love.graphics.rectangle("fill", self.x, self.y - (self.height / 2), self.width, self.height)
 end
