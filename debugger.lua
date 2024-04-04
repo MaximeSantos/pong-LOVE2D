57dc32575fd310fd69d07ee1ce8ae410826f8609
MaximeSantos <118924091+MaximeSantos@users.noreply.github.com>
2024-04-04 15:23:11 +0200

    Add debugger class with append and clear methods

---
+++ debugger.lua
@@ -0,0 +1,24 @@
+Debugger = {
+   msg
+}
+
+-- Helper functions
+function Debugger:append(string)
+   self.msg = self.msg .. " " .. string
+end
+
+function Debugger:clear(string)
+   self.msg = ""
+end
+
+-- Core functions
+function Debugger:load()
+   self.msg = ""
+end
+
+function Debugger:update(dt)
+end
+
+function Debugger:draw()
+   love.graphics.printf(self.msg, 0, (love.graphics.getHeight() - FONT_SIZE), love.graphics.getWidth(), "center")
+end
